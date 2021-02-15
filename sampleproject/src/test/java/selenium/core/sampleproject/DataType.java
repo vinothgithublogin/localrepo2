ae06a6e1191437de068f839425403c7e106a671b
vinothgithuglogin <vinoth.natarajan6343@gmail.com>
2021-02-16 01:03:53 +0530

    16/02

---
+++ sampleproject/src/test/java/selenium/core/sampleproject/DataType.java
@@ -0,0 +1,20 @@
+package selenium.core.sampleproject;
+
+public class DataType 
+{
+	public static void main(String[] yet) 
+	{
+		String role="Java Lead";
+		Short experience=6;
+		String curOrg="Accenture";
+		Float curCtc=6.7F;
+		String preLocation="Chennai";
+		System.out.println("Zealous Tech Corp");
+		System.out.println("Review your submission");
+		System.out.println("Role: "+role);
+		System.out.println("Experience: "+experience);
+		System.out.println("Current Organisation: "+curOrg);
+		System.out.println("Current CTC: "+curCtc);
+		System.out.println("Preferred location to work: "+preLocation);
+	}
+}
